2ccb90d7b53b977d5e35376b73cb9836b68074e8
ziy33 <59201468+ziy33@users.noreply.github.com>
2024-04-18 17:54:13 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           NextChat
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          If can't use, please email Ziy_Yan@intra.pegatroncorp.com/tonks.ziyyan@foxmail.com
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
